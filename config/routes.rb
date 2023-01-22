a6b225372d4053f5d0b38b71b9470f812821cafa
Jean <jean.mail92@gmail.com>
2023-01-22 13:49:51 +0100

    Final deploy

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+	get '/test', to: 'welcome#test'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
